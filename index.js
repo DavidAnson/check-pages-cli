985a6dd7138b8ed03110fb4ba537dbad28e9273d
David Anson <david@dlaa.me>
2016-04-19 22:00:45 -0700

    Complete implementation.

--- index.js
+++ index.js
@@ -1,4 +1,90 @@
 "use strict";
 
+var chalk = require("chalk");
 var checkPages = require("check-pages");
+var process = require("process");
 var yargs = require("yargs");
+var packageJson = require("./package.json");
+
+var options = yargs.
+  usage("Usage: $0 <page URLs> [options]").
+  option("checkLinks", {
+    "describe": "Validates each link on a page",
+    "type": "boolean"
+  }).
+  option("linksToIgnore", {
+    "describe": "List of URLs to ignore",
+    "type": "array"
+  }).
+  option("noEmptyFragments", {
+    "describe": "Fails for empty fragments",
+    "type": "boolean"
+  }).
+  option("noLocalLinks", {
+    "describe": "Fails for local links",
+    "type": "boolean"
+  }).
+  option("noRedirects", {
+    "describe": "Fails for HTTP redirects",
+    "type": "boolean"
+  }).
+  option("onlySameDomain", {
+    "describe": "Ignores links to other domains",
+    "type": "boolean"
+  }).
+  option("queryHashes", {
+    "describe": "Verifies query string file hashes",
+    "type": "boolean"
+  }).
+  option("checkCaching", {
+    "describe": "Validates Cache-Control/ETag",
+    "type": "boolean"
+  }).
+  option("checkCompression", {
+    "describe": "Validates Content-Encoding",
+    "type": "boolean"
+  }).
+  option("checkXhtml", {
+    "describe": "Validates page structure",
+    "type": "boolean"
+  }).
+  option("summary", {
+    "describe": "Summarizes issues after running",
+    "type": "boolean"
+  }).
+  option("maxResponseTime", {
+    "describe": "Response timeout (milliseconds)",
+    "type": "number"
+  }).
+  option("userAgent", {
+    "describe": "Custom User-Agent header",
+    "type": "string"
+  }).
+  group(
+    ["checkLinks", "checkCaching", "checkCompression", "checkXhtml"],
+    "Checks:").
+  group(
+    ["linksToIgnore", "noEmptyFragments", "noLocalLinks", "noRedirects",
+     "onlySameDomain", "queryHashes"],
+    "checkLinks options:").
+  version().
+  help().
+  wrap(false).
+  epilog(packageJson.description + "\n" + packageJson.homepage).
+  demand(1).
+  strict().
+  argv;
+options.pageUrls = options._;
+var errorCount = 0;
+var host = {
+  "log": function log (msg) {
+    console.log(chalk.green(msg));
+  },
+  "error": function error (msg) {
+    console.error(chalk.red(msg));
+    errorCount += 1;
+  }
+};
+checkPages(host, options, function callback () {
+  process.exit(errorCount);
+});
